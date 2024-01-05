d850880aef892cc735ff825d5e676d25da92903b
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-01-05 17:58:34 +0800

    Отправка сообщений в телеграм

---
+++ app/Models/Telegram.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Telegram extends Model
+{
+    use HasFactory;
+
+    public static function send_admin_chat_message($arrayOfStrings = [])
+    {
+        foreach ($arrayOfStrings as $string) {
+            if (!is_string($string)) {
+                throw new \DomainException('Каждая строка в сообщении должна быть СТРОКОЙ');
+                return false;
+            }
+        }
+
+        $chatId = env('ADMIN_CHAT_ID');
+        $telegramBotToken = env('ADMIN_AUTH_BOT_TOKEN');
+        $text = implode("\r\n", $arrayOfStrings);
+
+        self::sendMessage($text, $chatId, $telegramBotToken);
+    }
+
+    /**
+     * Отправляет сообщение в телеграм
+     * @param string $text - строка сообщения
+     * @param string $chatId - id чата, куда бот будет отправлять сообщение
+     * @param string $telegramBotToken - токен бота, который будет отправлять сообщение
+     * @return void
+     */
+    private static function sendMessage(string $text, string $chatId, string $telegramBotToken): void
+    {
+        $ch = curl_init();
+        curl_setopt_array(
+            $ch,
+            array(
+                CURLOPT_URL => 'https://api.telegram.org/bot' . $telegramBotToken . '/sendMessage',
+                CURLOPT_POST => TRUE,
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_POSTFIELDS => array(
+                    'chat_id' => $chatId,
+                    'text' => $text,
+                ),
+            )
+        );
+    }
+}
