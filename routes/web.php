ffcadfa7be69013ad84723396e3e391e28149285
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-01-07 10:55:06 +0800

    лишний маршрут убрал

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,6 @@ Route::middleware('auth')->group(function () {
 // маршруты кабинета
 Route::prefix('cabinet')->middleware(['auth', 'verified', /*'checkCurrentUser'*/])->group(function() {
     Route::get('/', [CabinetController::class, 'index'])->name('cabinet_index');
-    Route::get('/1', [CabinetController::class, 'index'])->name('cabinet_index1');
 });
 
 require __DIR__.'/auth.php';
